90d551d644e6ae4266719c8ba65dd83608423c96
Felipe Leal <felipe@hanzo.com.br>
2022-01-18 16:48:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const imc = document.querySelector(".info-imc").textContent;
+const pacientes = document.querySelectorAll(".table-row");
+
+const botao = document.getElementById("botao-calcular");
+
+function calculaImc() {
+  for (let i = 0; i < pacientes.length; i++) {
+    console.log(pacientes[i]);
+    const nome = pacientes[i].querySelector(".info-nome").textContent;
+    const peso = parseInt(pacientes[i].querySelector(".info-peso").textContent);
+    const altura = parseFloat(pacientes[i].querySelector(".info-altura").textContent);
+    const gordura = pacientes[i].querySelector(".info-gordura").textContent;
+
+    let valorImc = peso / (altura * 2);
+    imprimeImc(valorImc, pacientes[i]);
+  }
+}
+
+function imprimeImc(imc, paciente) {
+  paciente.querySelector(".info-imc").innerHTML = imc.toFixed(2);
+}
+
+botao.addEventListener("click",function(){
+  calculaImc();
+});
